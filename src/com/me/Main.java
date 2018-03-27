0eca12c78c50deb10cb3a00d832485f647205fa4
patrickatcw <pj8392@yahoo.com>
2018-03-27 09:07:19 -0400

    work for dog in main results commented

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -5,7 +5,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-    }
+        //step 11 an instance
+        Dog dog = new Dog("Yorkie");
 
+        //step 12 calling methods
+        dog.breathe();
+        dog.eat();
+
+        //step 13 run
+        /*
+        results;
+        Breathe in, breathe out, repeat
+        Yorkie is eating
+         */
+
+        //step 14 create a bird class
+
+    }
 
 }
